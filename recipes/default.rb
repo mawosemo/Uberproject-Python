c66a80f315909abbe42d1f5ac2da72d3a460bc23
Michael Awosemo <MAwosemo@spartaglobal.com>
2019-08-19 15:10:39 +0100

    added the relevant code to add the plugins needed for pip

--- recipes/default.rb
+++ recipes/default.rb
@@ -12,3 +12,36 @@ end
 apt_update 'update_sources' do
     action :update
 end
+execute 'pip install Flask==0.10.1' do
+  command 'pip install Flask==0.10.1'
+end
+execute 'pip install Jinja2==2.7.3' do
+  command 'pip install Jinja2==2.7.3'
+end
+execute 'pip install MarkupSafe==0.23' do
+  command 'pip install MarkupSafe==0.23'
+end
+execute 'pip install Werkzeug==0.9.6' do
+  command 'pip install Werkzeug==0.9.6'
+end
+execute 'pip install gnureadline==6.3.3' do
+  command 'pip install gnureadline==6.3.3'
+end
+execute 'pip install itsdangerous==0.24' do
+  command 'pip install itsdangerous==0.24'
+end
+execute 'pip install rauth==0.7.0' do
+  command 'pip install rauth==0.7.0'
+end
+execute 'pip install requests==2.3.0' do
+  command 'pip install requests==2.3.0'
+end
+execute 'pip install wsgiref==0.1.2' do
+  command 'pip install wsgiref==0.1.2'
+end
+execute 'pip install gunicorn==18.0' do
+  command 'pip install pip install gunicorn==18.0'
+end
+execute 'Flask 0.10.1' do
+  command 'pip install Flask-SSLify==0.1.4'
+end
