35785394895c8758e1cdb7b557143bfccfd3509e
Michael Awosemo <MAwosemo@spartaglobal.com>
2019-08-19 10:14:02 +0100

    writing the unit test for the python cookbook

--- spec/unit/recipes/default_spec.rb
+++ spec/unit/recipes/default_spec.rb
@@ -16,14 +16,16 @@ describe 'Python::default' do
       expect { chef_run }.to_not raise_error
     end
   end
-
-  context 'When all attributes are default, on CentOS 7' do
-    # for a complete list of available platforms and versions see:
-    # https://github.com/chefspec/fauxhai/blob/master/PLATFORMS.md
-    platform 'centos', '7'
-
-    it 'converges successfully' do
-      expect { chef_run }.to_not raise_error
+    it 'should install python'do
+      expect(chef_run).to install_package "python"
     end
-  end
+    it 'should enable python'do
+      expect(chef_run).to enable_service "python"
+    end
+    it 'should be start python'do
+      expect(chef_run).to start_service "python"
+    end
+
+
+
 end
