003ffa8b9006925f88c1c225226fd962ff1497c0
Michael Awosemo <MAwosemo@spartaglobal.com>
2019-08-20 11:35:00 +0100

    completed python intergration test and completed a kitchen test

--- recipes/default.rb
+++ recipes/default.rb
@@ -3,30 +3,42 @@
 # Recipe:: default
 #
 # Copyright:: 2019, The Authors, All Rights Reserved.
+
+apt_update 'update_sources' do
+  action :update
+end
+
 package 'python' do
   action :install
 end
+
 package 'python-pip' do
   action :install
 end
-apt_update 'update_sources' do
-    action :update
+
+package 'libncurses5-dev' do
+  action :install
 end
 execute 'pip install Flask==0.10.1' do
   command 'pip install Flask==0.10.1'
 end
+
 execute 'pip install Jinja2==2.7.3' do
   command 'pip install Jinja2==2.7.3'
 end
+
 execute 'pip install MarkupSafe==0.23' do
   command 'pip install MarkupSafe==0.23'
 end
+
 execute 'pip install Werkzeug==0.9.6' do
   command 'pip install Werkzeug==0.9.6'
 end
+
 execute 'pip install gnureadline==6.3.3' do
   command 'pip install gnureadline==6.3.3'
 end
+
 execute 'pip install itsdangerous==0.24' do
   command 'pip install itsdangerous==0.24'
 end
@@ -40,7 +52,7 @@ execute 'pip install wsgiref==0.1.2' do
   command 'pip install wsgiref==0.1.2'
 end
 execute 'pip install gunicorn==18.0' do
-  command 'pip install pip install gunicorn==18.0'
+  command ' pip install gunicorn==18.0'
 end
 execute 'Flask 0.10.1' do
   command 'pip install Flask-SSLify==0.1.4'
--- spec/unit/recipes/default_spec.rb
+++ spec/unit/recipes/default_spec.rb
@@ -25,37 +25,37 @@ describe 'Python::default' do
     it 'should install python-pip'do
       expect(chef_run).to install_package "python-pip"
     end
-    it 'should install python plugin Flask 0.10.1'do
-      expect(chef_run).to run _excute 'pip install Flask==0.10.1'
+    it 'should install plugins'do
+      expect(chef_run).to run_execute 'pip install Flask==0.10.1'
     end
     it 'should install python plugin Jinja2==2.7.3'do
-      expect(chef_run).to run _excute 'pip install Jinja2==2.7.3'
+      expect(chef_run).to run_execute 'pip install Jinja2==2.7.3'
     end
     it 'should install python plugin MarkupSafe==0.23'do
-      expect(chef_run).to run _excute 'pip install MarkupSafe==0.23'
+      expect(chef_run).to run_execute 'pip install MarkupSafe==0.23'
     end
     it 'should install python plugin Werkzeug==0.9.6'do
-      expect(chef_run).to run _excute 'pip install Werkzeug==0.9.6'
+      expect(chef_run).to run_execute 'pip install Werkzeug==0.9.6'
     end
     it 'should install python plugin gnureadline==6.3.3'do
-      expect(chef_run).to run _excute 'pip install gnureadline==6.3.3'
+      expect(chef_run).to run_execute 'pip install gnureadline==6.3.3'
     end
     it 'should install python plugin itsdangerous==0.24'do
-      expect(chef_run).to run _excute 'pip install itsdangerous==0.24'
+      expect(chef_run).to run_execute 'pip install itsdangerous==0.24'
     end
     it 'should install python plugin rauth==0.7.0'do
-      expect(chef_run).to run _excute 'pip install rauth==0.7.0'
+      expect(chef_run).to run_execute 'pip install rauth==0.7.0'
     end
     it 'should install python plugin requests==2.3.0'do
-      expect(chef_run).to run _excute 'pip install requests==2.3.0'
+      expect(chef_run).to run_execute 'pip install requests==2.3.0'
     end
     it 'should install python plugin wsgiref==0.1.2'do
-      expect(chef_run).to run _excute 'pip install wsgiref==0.1.2'
+      expect(chef_run).to run_execute 'pip install wsgiref==0.1.2'
     end
     it 'should install python plugin gunicorn==18.0'do
-      expect(chef_run).to run _excute 'pip install gunicorn==18.0'
+      expect(chef_run).to run_execute 'pip install gunicorn==18.0'
     end
     it 'should install python plugin Flask-SSLify==0.1.4'do
-      expect(chef_run).to run _excute 'pip install Flask-SSLify==0.1.4'
+      expect(chef_run).to run_execute 'pip install Flask-SSLify==0.1.4'
     end
 end
